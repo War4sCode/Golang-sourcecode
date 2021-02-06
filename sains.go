063849e71dcb9b7feea8c26b68f742902a965c83
War4sCode <70041921+War4sCode@users.noreply.github.com>
2021-02-06 13:36:38 +0700

    Update sains.go

--- sains.go
+++ sains.go
@@ -10,23 +10,28 @@ func main(){
 	fmt.Printf("siapa namamu?")
 	fmt.Scan(&orang)
 	fmt.Printf(" ok, selamat datang di programku,kak" + orang)
-	var name string
-	fmt.Println("\n ini adalah perhitungan energi potensial")
-	fmt.Printf("siap lanjut? ")
-	fmt.Scan(&name)
-	fmt.Printf("ok\n\n")
-	sainsdanteknologi.EnergiPotensial()
-	var nama string
-	fmt.Println("\n ini adalah perhitungan energi kinetik")
-	fmt.Printf("siap lanjut? ")
-	fmt.Scan(&nama)
-	fmt.Printf("ok\n\n")
-	sainsdanteknologi.EnergiKinetik()
-	var jeneng string
-	fmt.Println("\n ini adalah perhitungan getaran")
-	fmt.Printf("siap lanjut? ")
-	fmt.Scan(&jeneng)
-	fmt.Printf("ok\n\n")
-	sainsdanteknologi.Frekuensi()
+	
+	var science int
+	fmt.Println("Apa yang igin dicari?(ketikkan angkanya saja)")
+	fmt.Println("/t1. daya")
+	fmt.Println("/t2. usaha")
+	fmt.Println("/t3. waktu")
+	fmt.Println("/t4. Gerak lurus beraturan-kecepatan")
+	fmt.Println("/t5. Energi Potensial")
+	fmt.Println("/t6. Energi Kinetik")
+	fmt.Println("/t7. Frekuensi")
+	fmt.Printf("masukkan angka yang dipilih")
+	fmt.Scan(&science)
+	
+	switch science{
+		case 1: sainsdanteknologi.Daya()
+		case 2: sainsdanteknologi.Usaha()
+		case 3: sainsdanteknologi.Waktu()
+		case 4: sainsdanteknologi.GLB_Fast()
+		case 5: sainsdanteknologi.EnergiPotensial()
+		case 6: sainsdanteknologi.EnergiKinetik()
+		case 7: sainsdanteknologi.Frekuensi()
+		default: sainsdanteknologi.Daya()
+	}
 }
 
