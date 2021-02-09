3eb80e308d3a5cece7ed05a01057eeb7527af37d
War4sCode <70041921+War4sCode@users.noreply.github.com>
2021-02-09 08:48:37 +0700

    Update sains.go
    draft-1
    nb: jika ada yang diubah lagi, akan saya update di sini

--- sains.go
+++ sains.go
@@ -20,17 +20,36 @@ func main(){
 	fmt.Println("/t5. Energi Potensial")
 	fmt.Println("/t6. Energi Kinetik")
 	fmt.Println("/t7. Frekuensi")
+	fmt.Println("/t8. konversi suhu celcius\n")
+	fmt.Println("/t9. konversi suhu Kelvin\n")
+	fmt.Println("/t10. konversi suhu fahrenheit\n")
+	fmt.Println("/t11. konversi suhu reamur\n")
 	fmt.Printf("masukkan angka yang dipilih")
 	fmt.Scan(&science)
 	
 	switch science{
-		case 1: sainsdanteknologi.Daya()
-		case 2: sainsdanteknologi.Usaha()
-		case 3: sainsdanteknologi.Waktu()
-		case 4: sainsdanteknologi.GLB_Fast()
-		case 5: sainsdanteknologi.EnergiPotensial()
-		case 6: sainsdanteknologi.EnergiKinetik()
-		case 7: sainsdanteknologi.Frekuensi()
+		case 1:
+		sainsdanteknologi.Daya()
+		case 2:
+		sainsdanteknologi.Usaha()
+		case 3: 
+		sainsdanteknologi.Waktu()
+		case 4: 
+		sainsdanteknologi.GLB_Fast()
+		case 5: 
+		sainsdanteknologi.EnergiPotensial()
+		case 6: 
+		sainsdanteknologi.EnergiKinetik()
+		case 7: 
+		sainsdanteknologi.Frekuensi()
+		case 8:
+		sainsdanteknologi.Celcius()
+		case 9:
+		sainsdanteknologi.Kelvin()
+		case 10:
+		sainsdanteknologi.Fahrenheit()
+		case 11:
+		sainsdanteknologi.Reamur()
 		default: sainsdanteknologi.Daya()
 	}
 }
